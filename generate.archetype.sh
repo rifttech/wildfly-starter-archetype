ff32a6bf638110af294a93455bba135606b60f20
Abramov Arthur <rifttech.arthur@gmail.com>
2020-03-08 20:30:44 +0300

    add - scaffolding for the project:zap:

---
+++ generate.archetype.sh
@@ -0,0 +1,7 @@
+
+mvn archetype:generate                             \
+  -DarchetypeGroupId=com.github.rifttech           \
+  -DarchetypeArtifactId=wildfly-starter-achetype   \
+  -DarchetypeVersion=0.1.0-SNAPSHOT                \
+  -DgroupId=com.acme.wf                           \
+  -DartifactId=template
