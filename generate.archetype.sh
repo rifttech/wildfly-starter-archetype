98278c538a50273adaa6522110cdc193e0c73d20
Abramov Arthur <rifttech.arthur@gmail.com>
2020-03-10 09:00:15 +0300

    update bash script

--- generate.archetype.sh
+++ generate.archetype.sh
@@ -1,4 +1,20 @@
 
+#!/bin/sh
+
+
+FOLDER_NAME=archetype-impl
+TEMP_FOLDER=$("pwd")/$FOLDER_NAME
+
+if [ -d $TEMP_FOLDER ] 
+then
+  rm -rf $TEMP_FOLDER
+  
+fi
+
+
+mkdir $TEMP_FOLDER
+cd $FOLDER_NAME
+
 mvn archetype:generate                                \
   -DarchetypeGroupId=com.github.rifttech              \
   -DarchetypeArtifactId=wildfly-starter-archetype     \
@@ -7,3 +23,5 @@ mvn archetype:generate                                \
   -DartifactId=project-sample                         \
   -Dversion=0.1.0-SNAPSHOT                            \
   -DinteractiveMode=false
+
+cd ..
